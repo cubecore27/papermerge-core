5396a618d35a47051ffe39d69178cd402dfa47de
mrccdric27 <cubecore27@gmail.com>
2025-08-20 20:40:13 +0800

    Update environment variables and user creation script for Papermerge

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@ poetry run task server
 yarn workspace ui dev
 poetry run paper-cli users create --username admin --password admin --superuser
 poetry run paper-cli perms sync
+poetry run paper-cli index-schema apply
